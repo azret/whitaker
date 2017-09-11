19e2fd132d1dfb35cc53f4c1b44fc2841d0f5051
Azret Botash <azret.botash@gmail.com>
2017-09-11 13:17:23 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whitake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
